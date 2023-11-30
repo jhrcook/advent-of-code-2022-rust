4541ee0b430d2c2e306a044477c09cf7f165cce3
Joshua Cook <joshua_cook@vrtx.com>
2023-11-30 14:51:08 -0500

    feat: day 12

--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,6 +1,9 @@
-use std::collections::{HashMap, HashSet};
-
 use crate::data::load_raw;
+use petgraph::algo::k_shortest_path;
+use petgraph::graph::DiGraph;
+use petgraph::graph::NodeIndex;
+use std::collections::HashMap;
+use std::hash::Hash;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq, Eq)]
@@ -11,6 +14,10 @@ pub enum PuzzleError {
     UnknownHeight(String),
     #[error("No start coordinate.")]
     NoStartCoord,
+    #[error("No end coordinate.")]
+    NoEndCoord,
+    #[error("No paths found.")]
+    NoPathsFound,
 }
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -25,7 +32,7 @@ impl std::fmt::Display for Coord {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Height {
     Start(usize),
     End(usize),
@@ -62,15 +69,6 @@ impl HeightMap {
         }
     }
 
-    fn start_coord(&self) -> Result<Coord, PuzzleError> {
-        for (coord, value) in self.heights.iter() {
-            if let Height::Start(_) = value {
-                return Ok(*coord);
-            }
-        }
-        Err(PuzzleError::NoStartCoord)
-    }
-
     fn add_value(&mut self, row: usize, col: usize, value: Height) {
         log::debug!("Adding value: [{},{}] -> {}", row, col, value);
         self.heights.insert(Coord { r: row, c: col }, value);
@@ -78,27 +76,138 @@ impl HeightMap {
 }
 
 struct HeightTranslator {
-    score_map: HashMap<char, Height>,
+    score_map: HashMap<char, usize>,
 }
 
 impl HeightTranslator {
     fn new() -> Self {
         let mut score_map = HashMap::new();
         for (i, c) in ('a'..='z').enumerate() {
-            score_map.insert(c, Height::H(i));
+            score_map.insert(c, i);
         }
         Self { score_map }
     }
 
     fn translate(&self, input: &char) -> Result<Height, PuzzleError> {
         match input {
-            'S' => Ok(Height::Start(0)),
-            'E' => Ok(Height::End(25)),
-            x => self
-                .score_map
-                .get(x)
-                .cloned()
-                .ok_or(PuzzleError::UnknownHeight(input.to_string())),
+            'S' => Ok(Height::Start(
+                self.score_map
+                    .get(&'a')
+                    .cloned()
+                    .ok_or(PuzzleError::UnknownHeight(input.to_string()))?,
+            )),
+            'E' => Ok(Height::End(
+                self.score_map
+                    .get(&'z')
+                    .cloned()
+                    .ok_or(PuzzleError::UnknownHeight(input.to_string()))?,
+            )),
+            x => {
+                let val = self
+                    .score_map
+                    .get(x)
+                    .cloned()
+                    .ok_or(PuzzleError::UnknownHeight(input.to_string()))?;
+                Ok(Height::H(val))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Position {
+    height: Height,
+    coord: Coord,
+}
+
+struct Nodes {
+    nodes: HashMap<Position, NodeIndex>,
+}
+
+impl Nodes {
+    fn new() -> Self {
+        Nodes {
+            nodes: HashMap::new(),
+        }
+    }
+
+    fn get(&mut self, pos: &Position, tree: &mut DiGraph<Position, &str>) -> NodeIndex {
+        match self.nodes.get(pos) {
+            Some(i) => *i,
+            None => {
+                let i = tree.add_node(pos.clone());
+                self.nodes.insert(pos.clone(), i);
+                i
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct HeightTree<'a> {
+    tree: DiGraph<Position, &'a str>,
+    start: NodeIndex,
+    end: NodeIndex,
+}
+
+impl<'a> HeightTree<'a> {
+    fn from_height_map(height_map: &HeightMap) -> Result<Self, PuzzleError> {
+        let mut tree = DiGraph::new();
+        let mut start: Option<NodeIndex> = Option::None;
+        let mut end: Option<NodeIndex> = Option::None;
+        let mut nodes = Nodes::new();
+
+        for (coord, height) in height_map.heights.iter() {
+            let p = Position {
+                height: *height,
+                coord: *coord,
+            };
+            let node_idx = nodes.get(&p, &mut tree);
+
+            // Record Start and End nodes.
+            match height {
+                Height::Start(_) => start = Some(node_idx),
+                Height::End(_) => {
+                    end = Some(node_idx);
+                    continue;
+                }
+                _ => (),
+            }
+
+            for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)].iter() {
+                let new_r = coord.r as isize + dir.0;
+                let new_c = coord.c as isize + dir.1;
+                if (new_r < 0) | (new_c < 0) {
+                    continue;
+                }
+                let neighbor_coord = Coord {
+                    r: new_r as usize,
+                    c: new_c as usize,
+                };
+                if let Some(neighbor_height) = height_map.heights.get(&neighbor_coord) {
+                    if neighbor_height.get_height() <= (height.get_height() + 1) {
+                        let neighbor_pos = Position {
+                            height: *neighbor_height,
+                            coord: neighbor_coord,
+                        };
+                        let neighbor_idx = nodes.get(&neighbor_pos, &mut tree);
+                        tree.add_edge(node_idx, neighbor_idx, "");
+                    }
+                }
+            }
+        }
+        Ok(HeightTree {
+            tree,
+            start: start.ok_or(PuzzleError::NoStartCoord)?,
+            end: end.ok_or(PuzzleError::NoEndCoord)?,
+        })
+    }
+
+    fn shortest_distance(&self) -> Result<usize, PuzzleError> {
+        let search_results = k_shortest_path(&self.tree, self.start, Some(self.end), 1, |_| 1);
+        match search_results.get(&self.end) {
+            Some(dist) => Ok(*dist),
+            None => Err(PuzzleError::NoPathsFound),
         }
     }
 }
@@ -114,83 +223,25 @@ fn parse_input(input_data: &str) -> Result<HeightMap, PuzzleError> {
     Ok(heightmap)
 }
 
-#[derive(Debug, Clone)]
-struct HeightTree {
-    tree: HashMap<Coord, HashSet<Coord>>,
-}
-
-impl HeightTree {
-    fn new() -> Self {
-        HeightTree {
-            tree: HashMap::new(),
-        }
-    }
-    fn from_height_map(height_map: &HeightMap) -> Self {
-        let mut tree = HeightTree::new();
-        for (current_coord, current_height) in height_map.heights.iter() {
-            log::debug!(
-                "Adding children for coord {} with value {}",
-                current_coord,
-                current_height
-            );
-
-            // Skip if End node.
-            if let Height::End(_) = current_height {
-                log::debug!("Skipping End node.");
-                continue;
-            }
-
-            // Compare current coord's height against neighbors' heights.
-            let mut neighbors = HashSet::new();
-            for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)].iter() {
-                let new_r = current_coord.r as isize + dir.0;
-                let new_c = current_coord.c as isize + dir.1;
-                if (new_r < 0) | (new_c < 0) {
-                    continue;
-                }
-                let neighbor_coord = Coord {
-                    r: new_r as usize,
-                    c: new_c as usize,
-                };
-                match height_map.heights.get(&neighbor_coord) {
-                    Some(Height::End(_)) | None => (),
-                    Some(Height::Start(x)) | Some(Height::H(x)) => {
-                        if x <= &(current_height.get_height() + 1) {
-                            neighbors.insert(neighbor_coord);
-                        }
-                    }
-                }
-            }
-            log::debug!("  children: {:?}", neighbors);
-            tree.tree.insert(*current_coord, neighbors);
-        }
-        tree
-    }
-}
-
-impl std::fmt::Display for HeightTree {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "----- HEIGHT TREE -----")?;
-        for (node, children) in self.tree.iter() {
-            let children_str: String = children
-                .iter()
-                .map(|c| c.to_string())
-                .collect::<Vec<_>>()
-                .join(", ");
-            writeln!(f, "{}: {}", node, children_str)?
-        }
-        write!(f, "--------------------")
-    }
-}
-
 pub fn puzzle_1(input_data: &str) -> Result<usize, PuzzleError> {
-    log::info!("Building Height Map..");
-    let height_map = parse_input(input_data)?;
-    log::info!("Building Height Tree.");
-    let height_tree = HeightTree::from_height_map(&height_map);
-    log::info!("Finished building Tree.");
-    log::debug!("{}", height_tree);
-    Ok(0)
+    let height_tree = HeightTree::from_height_map(&parse_input(input_data)?)?;
+    height_tree.shortest_distance()
+}
+
+pub fn puzzle_2(input_data: &str) -> Result<usize, PuzzleError> {
+    let mut height_tree = HeightTree::from_height_map(&parse_input(input_data)?)?;
+    height_tree.tree.reverse(); // Reverse and go from E to all nodes with height "a".
+    height_tree
+        .tree
+        .node_indices()
+        .filter(|i| height_tree.tree[*i].height.get_height() == 0)
+        .filter_map(|i| {
+            k_shortest_path(&height_tree.tree, height_tree.end, Some(i), 1, |_| 1)
+                .get(&i)
+                .cloned()
+        })
+        .min()
+        .ok_or(PuzzleError::NoPathsFound)
 }
 
 pub fn main(data_dir: &str) {
@@ -206,17 +257,17 @@ pub fn main(data_dir: &str) {
     assert_eq!(answer_1, Ok(447));
 
     // Puzzle 2.
-    // let answer_2 = puzzle_2(&data, 10000);
-    // match &answer_2 {
-    //     Ok(x) => println!(" Puzzle 2: {}", x),
-    //     Err(e) => panic!("Error on Puzzle 2: {}", e),
-    // }
-    // assert_eq!(answer_2, Ok(29703395016));
+    let answer_2 = puzzle_2(&data);
+    match &answer_2 {
+        Ok(x) => println!(" Puzzle 2: {}", x),
+        Err(e) => panic!("Error on Puzzle 2: {}", e),
+    }
+    assert_eq!(answer_2, Ok(446));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solutions::day12::puzzle_1;
+    use crate::solutions::day12::{puzzle_1, puzzle_2};
 
     const EXAMPLE_1: &str = "
     Sabqponm
@@ -227,9 +278,16 @@ mod tests {
     ";
 
     #[test]
-    fn puzzle_1_example() {
+    fn puzzle_1_example_1() {
         env_logger::init();
         let res = puzzle_1(EXAMPLE_1);
         assert_eq!(res, Ok(31));
+    }
+
+    #[test]
+    fn puzzle_2_example_1() {
+        env_logger::init();
+        let res = puzzle_2(EXAMPLE_1);
+        assert_eq!(res, Ok(29));
     }
 }
